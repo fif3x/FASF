199cb394a0faa3679937379667305b6b9861bd28
fif3x <185714407+fif3x@users.noreply.github.com>
2025-05-15 19:44:38 +0300

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,16 +1,18 @@
 #include <vector>
-#include "FSAF.hpp"
+#include "FSAF.hpp" //include all needed libraries
 #include <string>
 
 int main() {
 
-	std::vector<std::string> carBrands = {"Ford", "BMW", "Fiat", "Tesla", "Ferrari", "Renault", "Toyota"};
+	//To test the example code, you may replace a comment with actual logic, for example the comment with the vector "carBrands" and the comment saying "FSAF::vector::scan<std::string>(carBrands, "Fiat");"
 
-	std::vector<int> integerArray{ 1, 3, 199, 781, 5151, 5643, 0 };
+	//std::vector<std::string> carBrands = {"Ford", "BMW", "Fiat", "Tesla", "Ferrari", "Renault", "Toyota"};
 
-	//FSAF::vector::scan<std::string>(carBrands, "Fiat");
+	//std::vector<int> integerArray{ 1, 3, 199, 781, 5151, 5643, 0 };
 
-	FSAF::vector::scan<int>(integerArray, 781);
+	//FASF::vector::scan<std::string>(carBrands, "Fiat");
+
+	//FASF::vector::scan<int>(integerArray, 781);
 
 	return 0;
 }
